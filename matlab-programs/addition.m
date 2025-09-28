e3e3f228a12c0336b9d88d248981f75eb4b120e4
pavan <pk7057764@gmail.com>
2025-09-28 15:30:06 +0530

    Update addition.m
    all done conflicts removed

--- matlab-programs/addition.m
+++ matlab-programs/addition.m
@@ -4,6 +4,5 @@ P = input('Enter third number: ');
 C = input('Enter thirrd number: ');
 Q = input('Enter foruth number: ');
 sum = A + B + C + Q + p;
->>>>>>> a35a467ae938ed04f3427eb764e9db27ce1f5024
 disp(['Sum: ', num2str(sum)]);
 
